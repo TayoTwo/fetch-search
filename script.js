14388a6fe50fd4ca7b1d9d7eb7eb6d68c38bd053
Glitch (hello-webpage) <none>
2019-07-01 10:09:10 +0000

    🐒🐛 Checkpoint ./script.js:5088056/867

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 const ulist = document.getElementById('main__list');
 
+function addItems(){
+  
+  const data = fetch('https://picsum.photos/v2/list?page=2&limit=100')
+  .then(function(response) {
+    return response.json();
+  });
+  
+  console.log(data.download_url);
+  
+}
+
 function main(evt) {
   "use strict";
+  
+  addItems();
 
   const regex = RegExp(`^${evt.target.value}`, 'i');
   
   const listItems = [...ulist.getElementsByTagName('li')]
     .forEach(item => {
       
-      console.log(item.innerText);
       item.style.display = `${item.innerText}`
         .split(/[^a-z]/ig)
         .some(w => regex.test(w)) ? "" : "none"; 
