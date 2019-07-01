62058d9b0c1bc8541f6d90247a26ad32c77da032
Glitch (hello-webpage) <none>
2019-07-01 13:26:05 +0000

    ⛱🏉 Checkpoint ./script.js:5088056/3080

--- script.js
+++ script.js
@@ -2,62 +2,63 @@ const ulist = document.getElementById('main__list');
 
 main();
 
-async function requestImages(){
-  
+async function requestImages() {
+
   const data = await (
     fetch('https://picsum.photos/v2/list?page=2&limit=100')
-      .then(response => response.json())
-      .then(data => data.map(photo => photo.download_url))
+    .then(response => response.json())
+    .then(data => data.map(photo => photo.download_url))
   );
-  
+
   console.log(data);
   return data;
 }
 
-async function requestAuthorNames(){
-  
+async function requestAuthorNames() {
+
   const data = await (
     fetch('https://picsum.photos/v2/list?page=2&limit=100')
-      .then(response => response.json())
-      .then(data => data.map(photo => photo.author))
+    .then(response => response.json())
+    .then(data => data.map(photo => photo.author))
   );
-  
+
   console.log(data);
   return data;
 }
 
 async function main(evt) {
   "use strict";
-  
+
   const images = await requestImages();
   const authors = await requestAuthorNames();
-  
- for(let j = 0;j < images.length;j++){
-    
-    var node = document.createElement("li");   
+
+  console.log(images.length);
+
+  for (let j = 0; j < images.length; j++) {
+
+    var node = document.createElement("li");
     var i = document.createElement("img");
     var p = document.createElement("p");
     node.className += "main__list-item";
-   
-    i.src =  images[j];
+
+    i.src = images[j];
     p.innerText = authors[j];
-   
+
     node.appendChild(i);
     node.appendChild(p);
     document.getElementById("main__list").appendChild(node);
-    
+
   }
-    
-    
+
   const regex = RegExp(`^${evt.target.value}`, 'i');
-  
+
   const listItems = [...ulist.getElementsByTagName('li')]
     .forEach(item => {
-      
+
       item.style.display = `${item.innerText}`
         .split(/[^a-z]/ig)
-        .some(w => regex.test(w)) ? "" : "none"; 
-      
+        .some(w => regex.test(w)) ? "" : "none";
+
     });
-  
+
 }
